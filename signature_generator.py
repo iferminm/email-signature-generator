7a0e8d0268a414da0fbf0ae733e0905101e4f7d3
Israel Fermín Montilla <iferminm@gmail.com>
2013-03-05 12:54:55 -0430

    Esqueleto terminado, faltan plantillas

--- signature_generator.py
+++ signature_generator.py
@@ -1,3 +1,26 @@
 #coding=utf-8
 from __future__ import unicode_literals
+import ConfigParser
 import cherrypy
+import gspread
+
+class Home:
+    def __init__(self):
+        config = ConfigParser.ConfigParser()
+        config.read('config.ini')
+        self.key = config.get('spreadsheet', 'key')
+        self.login = config.get('account', 'login')
+        self.passwd = config.get('account', 'password')
+
+    @cherrypy.expose
+    def index(self, email=None):
+        gc = gspread.login(self.login, self.passwd)
+        wks = gc.open_by_key(self.key)
+        worksheet = wks.get_worksheet(0)
+        try:
+            rownumber = worksheet.find(email).row
+            return worksheet.row_values(rownumber)
+        except:
+            return "Not Found %s" % email
+
+cherrypy.quickstart(Home())
