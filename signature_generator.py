5e959f309bc147f1c3ffb79bfcca2361f6ac95bb
Israel Fermín Montilla <iferminm@gmail.com>
2013-03-05 16:37:40 -0430

    Montaje finalizado

--- signature_generator.py
+++ signature_generator.py
@@ -1,16 +1,22 @@
 #coding=utf-8
 from __future__ import unicode_literals
+from jinja2 import Environment, FileSystemLoader
+env = Environment(loader=FileSystemLoader('html'))
 import ConfigParser
 import cherrypy
 import gspread
 
-class Home:
+class SignatureGenerator:
     def __init__(self):
         config = ConfigParser.ConfigParser()
         config.read('config.ini')
         self.key = config.get('spreadsheet', 'key')
         self.login = config.get('account', 'login')
         self.passwd = config.get('account', 'password')
+
+    def serve_template(self, template_name, **kwargs):
+        template = env.get_template(template_name)
+        return template.render(**kwargs)
 
     @cherrypy.expose
     def index(self, email=None):
@@ -21,6 +27,6 @@ class Home:
             rownumber = worksheet.find(email).row
             return worksheet.row_values(rownumber)
         except:
-            return "Not Found %s" % email
+            return self.serve_template('404.html')
 
-cherrypy.quickstart(Home())
+cherrypy.quickstart(SignatureGenerator(), '/', 'generator.cfg')
